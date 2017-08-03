bba7c14996a119c7e0c6b9cffe98eebf6d4dca7b
olaHalo <30677521+olaHalo@users.noreply.github.com>
2017-08-03 15:07:03 -0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -24,6 +24,12 @@ namespace KegBot
         [DllImport("user32.dll", SetLastError = true)]
         static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
 
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool IsIconic(IntPtr hWnd);
+
         //const uint SWP_NOSIZE = 0x0001;
         
         const uint SWP_NOZORDER = 0x0004;
